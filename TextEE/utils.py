e410b9fba81c8a4e4b2a92995952faa388d48dba
Truc Hoang <hoangthuytruc@gmail.com>
2024-07-02 17:52:03 +0700

    remove LLM options in trainers

--- TextEE/utils.py
+++ TextEE/utils.py
@@ -32,8 +32,8 @@ TRAINER_MAP = {
     ("BartGen", "EAE"): BartGenEAETrainer,
     ("Ampere", "EAE"): AmpereEAETrainer,
     ("AMRIE", "E2E"): AMRIEE2ETrainer,
-    ("LLM", "ED"): LLMEDTrainer,
-    ("LLM", "EAE"): LLMEAETrainer,
+    #("LLM", "ED"): LLMEDTrainer,
+    #("LLM", "EAE"): LLMEAETrainer,
 }
 
 def load_config(config_fn):
